0ab7f60f15b388e8738882929909c27851f23359
alexgargui <garciaguillenalejandro@gmail.com>
2021-07-14 21:05:40 -0500

    feat(algorithms): Bfs

--- bfs-dfs/max-area-of-island.js
+++ bfs-dfs/max-area-of-island.js
@@ -1,6 +1,6 @@
 /**
- * Problem: Island perimeter
- * Link to problem: https://leetcode.com/problems/island-perimeter
+ * Problem: Max area of island
+ * Link to problem: https://leetcode.com/problems/max-area-of-island
  * Coder: alexgargui
  * @param {number[][]} grid
  * @return {number}
@@ -14,31 +14,32 @@ const mobility = [
 ];
 
 const validIsland = (grid, row, column) => {
-  grid[row][column] = -1;
+  grid[row][column] = 0;
+  total += 1;
   mobility.forEach((position) => {
     const newRow = row + position[0];
     const newColumn = column + position[1];
     if (newRow >= 0 && newRow < grid.length) {
       if (newColumn >= 0 && newColumn < grid[0].length) {
-        if (grid[newRow][newColumn] === 1) validIsland(grid, newRow, newColumn);
-        else if (grid[newRow][newColumn] === 0) total += 1;
-      } else {
-        total += 1;
+        if (grid[newRow][newColumn] === 1) {
+          validIsland(grid, newRow, newColumn);
+        }
       }
-    } else {
-      total += 1;
     }
   });
 };
 
-const islandPerimeter = (grid) => {
+const maxAreaOfIsland = (grid) => {
   total = 0;
+  let result = 0;
   for (let row = 0; row < grid.length; row++) {
     for (let column = 0; column < grid[0].length; column++) {
       if (grid[row][column] === 1) {
         validIsland(grid, row, column);
+        if (total > result) result = total;
+        total = 0;
       }
     }
   }
-  return total;
+  return result;
 };
