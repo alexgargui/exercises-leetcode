b6ddbe7c582be30bc83712f4010ab0b1ca8d3e83
alexgargui <garciaguillenalejandro@gmail.com>
2023-08-20 19:30:56 -0600

    Number of provinces

---
+++ bfs-dfs/number-of-provinces.js
@@ -0,0 +1,30 @@
+/**
+ * Problem: Number of provinces
+ * Link to problem: https://leetcode.com/problems/number-of-provinces
+ * Coder: alexgargui
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+let dfs = ((isConnected, fila) => {
+    console.log(fila);
+    for (let j = 0; j < isConnected.length; j++) {
+        if (isConnected[fila][j] == 1) {
+            isConnected[fila][j] = 0;
+            dfs(isConnected, j);
+        }
+    }
+});
+
+var findCircleNum = function(isConnected) {
+    let cont = 0;
+    for (let i = 0; i < isConnected.length; i++) {
+        for (let j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1) {
+                isConnected[i][j] = 0;
+                cont++;
+                dfs(isConnected, j);
+            }
+        }
+    }
+    return cont;
+};
