f3144025e8b811bd60d66fcf1e6ad9a5e479d580
alexgargui <garciaguillenalejandro@gmail.com>
2023-08-21 22:35:55 -0600

    Maximum 69 number

---
+++ basics/maximum-69-number.js
@@ -0,0 +1,8 @@
+/**
+ * Problem: Maximum 69 number
+ * Link to problem: https://leetcode.com/problems/can-place-flowers
+ * Coder: alexgargui
+ * @param {number} num
+ * @return {number}
+ */
+const maximum69Number  = num => num.toString().replace('6', '9');
